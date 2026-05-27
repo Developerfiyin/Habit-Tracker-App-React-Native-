65ecf7e9369d3bca47ac9f0079037549eb00bd4e
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-05-27 20:21:39 +0100

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -7,6 +7,7 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "orange",
       }}
     >
       <Text>Hello, React Native! How are you doing ?  </Text>
