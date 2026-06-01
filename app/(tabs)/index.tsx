c5672506da9a314b59af58cf84b8a55fb43696b0
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-06-02 00:22:37 +0100

    import stylesheet and used it

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,29 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "orange",
-      }}
-    >
+    <View style={style.view}>
       <Text>Hello, React Native! How are you doing ? </Text>
       <Text> This is the default first screen </Text>
-      <Link
-        href="/login"
-        style={{
-          width: 100,
-          height: 10,
-          backgroundColor: "tint",
-          borderRadius: 8,
-          textAlign: "center",
-        }}
-      >
+      <Link href="/login" style={style.link}>
         {" "}
         Login page
       </Link>
     </View>
   );
 }
+const style = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "orange",
+  },
+  link: {
+    width: 100,
+    height: 10,
+    backgroundColor: "tint",
+    borderRadius: 8,
+    textAlign: "center",
+  },
+});
