bfd42df2fdf1457ca32a78c10626913a436ed139
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-06-01 09:31:31 +0100

    Update login.tsx file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,10 @@
 import { Stack } from "expo-router";
 
-export default function TabsLayout() {
+export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="login" options={{headerShown: false}} />
+      <Stack.Screen name="index" options={{ title: "Home", headerShown: false }} />
+      <Stack.Screen name="login" options={{title: "Login", headerShown: false}} />
     </Stack>
   );
 }
--- app/login.tsx
+++ app/login.tsx
@@ -3,7 +3,7 @@ import { View,Text } from "react-native";
 export default function LoginScreen() {
     return (
         <View> 
-            <Text> OKAY NAW</Text>
+            <Text> OKAY NAW This is the login screen.</Text>
         </View>
     )
 }
