11ceb654b968018ef1ce7fe03df127b2a19cf502
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-06-01 09:41:39 +0100

    style link

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import { Text, View } from "react-native";
 import { Link } from "expo-router";
+import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
@@ -10,9 +10,21 @@ export default function Index() {
         backgroundColor: "orange",
       }}
     >
-      <Text>Hello, React Native! How are you doing ?  </Text>
+      <Text>Hello, React Native! How are you doing ? </Text>
       <Text> This is the default first screen </Text>
-      <Link href={"/_sitemap"}> Login page</Link>
+      <Link
+        href="/login"
+        style={{
+          width: 100,
+          height: 50,
+          backgroundColor: "tint",
+          borderRadius: 8,
+          textAlign: "center",
+        }}
+      >
+        {" "}
+        Login page
+      </Link>
     </View>
   );
 }
