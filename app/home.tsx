478b10c2f24e7e1c1eb9b17f5fc1b2e482f763fe
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-06-02 23:45:41 +0100

    Add comment to code lines

--- app/home.tsx
+++ app/home.tsx
@@ -1,60 +1,60 @@
-import { router } from "expo-router";
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+// import { router } from "expo-router";
+// import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
-export default function HomeScreen() {
-  return (
-    <View
-      style={{
-        flexDirection: "column",
-        gap: 6,
-      }}
-    >
-      <View>
-        <Image
-          source={require("../assets/images/image 4.png")}
-          style={{ marginHorizontal: "auto", top: "auto", marginTop: 80 }}
-        />
-        <Image
-          source={require("../assets/images/Ellipse 1.png")}
-          style={{ marginHorizontal: "auto", bottom: 100 }}
-        />
-      </View>
+// export default function HomeScreen() {
+//   return (
+//     <View
+//       style={{
+//         flexDirection: "column",
+//         gap: 6,
+//       }}
+//     >
+//       <View>
+//         <Image
+//           source={require("../assets/images/image 4.png")}
+//           style={{ marginHorizontal: "auto", top: "auto", marginTop: 80 }}
+//         />
+//         <Image
+//           source={require("../assets/images/Ellipse 1.png")}
+//           style={{ marginHorizontal: "auto", bottom: 100 }}
+//         />
+//       </View>
 
-      <Text
-        style={{
-          fontWeight: "600",
-          color: "#0f172a",
-          fontSize: 32,
-          textAlign: "center",
-          marginBottom: 20,
-          marginHorizontal: "10%",
-        }}
-      >
-        Get the best service needed for your car here
-      </Text>As I reflect on God’s justice and mercy, I feel grateful that both qualities exist perfectly together in Heavenly Father’s plan. At first, justice can seem strict because it reminds me that my actions have consequences and that God expects me to live according to His commandments. Justice teaches accountability, honesty, and growth. It reminds me that God is fair and that He understands every choice, intention, and circumstance perfectly.\n\nAt the same time, I feel comforted by God’s mercy. I know that I make mistakes, fall short, and sometimes struggle spiritually, but because of Jesus Christ, I am not left without hope. Mercy gives me the opportunity to repent, improve, and continue moving forward instead of being permanently defined by my weaknesses or failures. It helps me understand that God’s love is patient and compassionate.\n\nOne impression I have gained is that justice and mercy are not opposites that compete against each other. Through the Atonement of Jesus Christ, they work together perfectly. Justice allows growth and accountability, while mercy allows healing and redemption. Without justice, there would be no true purpose in our choices, but without mercy, none of us could return to God because everyone makes mistakes.\n\nAnother impression I have is that God sees more than people see. Sometimes humans judge quickly based on outward actions, but God understands the heart, intentions, struggles, and efforts of every person. That thought gives me peace because it reminds me that God’s judgment is completely fair and perfectly loving.\n\nReflecting on God’s justice and mercy also makes me want to become more merciful toward others. Since Heavenly Father is patient with me as I learn and grow, I should also show patience, forgiveness, and understanding toward people around me
+//       <Text
+//         style={{
+//           fontWeight: "600",
+//           color: "#0f172a",
+//           fontSize: 32,
+//           textAlign: "center",
+//           marginBottom: 20,
+//           marginHorizontal: "10%",
+//         }}
+//       >
+//         Get the best service needed for your car here
+//       </Text>As I reflect on God’s justice and mercy, I feel grateful that both qualities exist perfectly together in Heavenly Father’s plan. At first, justice can seem strict because it reminds me that my actions have consequences and that God expects me to live according to His commandments. Justice teaches accountability, honesty, and growth. It reminds me that God is fair and that He understands every choice, intention, and circumstance perfectly.\n\nAt the same time, I feel comforted by God’s mercy. I know that I make mistakes, fall short, and sometimes struggle spiritually, but because of Jesus Christ, I am not left without hope. Mercy gives me the opportunity to repent, improve, and continue moving forward instead of being permanently defined by my weaknesses or failures. It helps me understand that God’s love is patient and compassionate.\n\nOne impression I have gained is that justice and mercy are not opposites that compete against each other. Through the Atonement of Jesus Christ, they work together perfectly. Justice allows growth and accountability, while mercy allows healing and redemption. Without justice, there would be no true purpose in our choices, but without mercy, none of us could return to God because everyone makes mistakes.\n\nAnother impression I have is that God sees more than people see. Sometimes humans judge quickly based on outward actions, but God understands the heart, intentions, struggles, and efforts of every person. That thought gives me peace because it reminds me that God’s judgment is completely fair and perfectly loving.\n\nReflecting on God’s justice and mercy also makes me want to become more merciful toward others. Since Heavenly Father is patient with me as I learn and grow, I should also show patience, forgiveness, and understanding toward people around me
 
-      <Pressable
-        style={styles.pressable}
-        onPress={() => router.push("/(tabs)/login")}
-      >
-        <Text style={styles.text1}> Get Started</Text>
-      </Pressable>
-    </View>
-  );
-}
+//       <Pressable
+//         style={styles.pressable}
+//         onPress={() => router.push("/(tabs)/login")}
+//       >
+//         <Text style={styles.text1}> Get Started</Text>
+//       </Pressable>
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  pressable: {
-    backgroundColor: "#0f172a",
-    marginTop: "10%",
-    padding: 20,
-    borderRadius: 50,
-    marginHorizontal: 20,
-  },
-  text1: {
-    color: "#ffff",
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   pressable: {
+//     backgroundColor: "#0f172a",
+//     marginTop: "10%",
+//     padding: 20,
+//     borderRadius: 50,
+//     marginHorizontal: 20,
+//   },
+//   text1: {
+//     color: "#ffff",
+//     fontSize: 20,
+//     fontWeight: "bold",
+//     textAlign: "center",
+//   },
+// });
