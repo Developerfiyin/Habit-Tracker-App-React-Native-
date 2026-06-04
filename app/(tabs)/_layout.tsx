24a51117209402eb3720f813ce85e1f68c431dc3
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-06-04 11:25:05 +0100

    Created metro.config.js

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ export default function TabsLayout() {
   return (
     <Tabs screenOptions={{tabBarActiveTintColor: "red"}}>
       <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="login" options={{title: "Login", headerShown: false}} />
+      <Tabs.Screen name="login" options={{title: "Login" }} />
     </Tabs>
   );
 }
