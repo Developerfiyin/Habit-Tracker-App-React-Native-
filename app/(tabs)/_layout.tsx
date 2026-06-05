a057493f910f8b889463837c6c3f3892322421a6
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-06-05 14:02:57 +0100

    Update layout inside tabs folder

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,18 +9,19 @@ export default function TabsLayout() {
         name="index"
         options={{
           title: "Home",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome5 name="home" size={24} color={color} />
-          ),
-        }}
+          tabBarIcon: ({ color, focused }) => {
+            return focused ? (
+              <FontAwesome5 name="home" size={24} color={color} />
+            ) : (
+              <AntDesign name="home" size={24} color={color} />
+            );
+          },
+        }} 
       />
       <Tabs.Screen
         name="login"
         options={{
           title: "Login",
-          tabBarIcon: ({ color }) => (
-            <AntDesign name="user" size={24} color={color} />
-          ),
         }}
       />
     </Tabs>
