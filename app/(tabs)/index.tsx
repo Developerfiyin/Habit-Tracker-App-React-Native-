2aafeecb5c7207935bdfdde9a2a8baffb37d593b
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-06-13 23:26:09 +0100

    Update tab screens

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,28 +1,28 @@
-// import { Link } from "expo-router";
-// import { StyleSheet, Text, View } from "react-native";
-// export default function Index() {
-//   return (
-//     <View style={style.view}>
-//       <Text>Hello, React Native! How are you doing ? </Text>
-//       <Text> This is the default first screen </Text>
-//       <Link href="/login" style={style.link}>
-//         {" "}
-//         {/* Login page */}
-//       </Link>
-//     </View>
-//   );
-// }
-// const style = StyleSheet.create({
-//   view: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//   },
-//   link: {
-//     width: 100,
-//     height: 10,
-//     backgroundColor: "tint",
-//     borderRadius: 8,
-//     textAlign: "center",
-//   },
-// });
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+export default function Index() {
+  return (
+    <View style={style.view}>
+      <Text>Hello, React Native! How are you doing ? </Text>
+      <Text> This is the default first screen </Text>
+      <Link href="/login" style={style.link}>
+        {" "}
+        {/* Login page */}
+      </Link>
+    </View>
+  );
+}
+const style = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  link: {
+    width: 100,
+    height: 10,
+    backgroundColor: "tint",
+    borderRadius: 8,
+    textAlign: "center",
+  },
+});
