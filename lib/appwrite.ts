c57440e73bc3ca5d89e973cd8a93c93427dbc544
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-06-15 19:25:07 +0100

    Update appwrite.ts

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,3 +1,7 @@
-import {Account, Databases, Client} from "react-native-appwrite"
+import { Client, Account } from "react-native-appwrite";
+const client = new Client()
+  .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
+  .setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!);
 
-const client = new Client().setEndpoint("https://fra.cloud.appwrite.io/v1").setProject("697737a8001e1a94ed29");
+  export const account = new Account(client);
