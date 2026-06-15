576fb1e193e3550215cf2f06a73df19fcb8f0e4b
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-06-15 11:59:32 +0100

    Update and installed polyfill

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,1 +1,3 @@
-import {Client} from " "
+import {Account, Databases, Client} from "react-native-appwrite"
+
+const client = new Client().setEndpoint("https://fra.cloud.appwrite.io/v1").setProject("697737a8001e1a94ed29");
