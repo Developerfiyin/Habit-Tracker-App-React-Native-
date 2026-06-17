b3c423fae6d53249b48b423bd8032c6b12d9abd0
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-06-17 21:49:21 +0100

    Update auuth.tsx

--- app/auth.tsx
+++ app/auth.tsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import { KeyboardAvoidingView, Platform, View } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
 export default function AuthScreen() {
-    const [email, setEmail] = useState<boolean>(false);
-    const [password, setPassword] = useState("");
+  const [isSignUp, setIsSignUp] = useState<boolean>(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSumbit = () => {
+    setIsSignUp((prev) => !prev);
+  };
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "android" ? "padding" : "height"}
     >
       <View>
-        <Text>Create Account</Text>
+        <Text>{isSignUp ? "Welcome Back!" : "Create Account"}</Text>
 
         <TextInput
           label="Email"
@@ -27,7 +34,6 @@ export default function AuthScreen() {
         />
         <Button mode="contained"> Sign Up</Button>
         <Button mode="text">Already have an account? Sign In</Button>
-
       </View>
     </KeyboardAvoidingView>
   );
