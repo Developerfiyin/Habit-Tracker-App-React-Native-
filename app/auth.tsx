f8d5885a432694e4467cb6e0cebf5843c1ebe38f
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-06-20 23:57:17 +0100

    Update auth.tsx

--- app/auth.tsx
+++ app/auth.tsx
@@ -95,7 +95,7 @@ const styles = StyleSheet.create({
   },
   text: {
     marginBottom: 24,
-    textAlign: "center",we keg rkg ek
+    textAlign: "center",
     fontSize: 24,
     fontWeight: "bold",
   },
